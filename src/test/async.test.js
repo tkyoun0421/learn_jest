94ef9525c6b5216ddedd2640935fff91a8d0bda4
태관 <tkyoun0421@naver.com>
2024-06-19 15:01:11 +0900

    async - return

--- src/test/async.test.js
+++ src/test/async.test.js
@@ -1,9 +1,15 @@
 const fetchProduct = require("../async");
 
 describe("Async", () => {
+  it("async-done", (done) => {
+    fetchProduct().then((item) => {
+      expect(item).toEqual({ item: "Milk", price: 2000 });
+      done();
+    });
+  });
   it("async-return", () => {
     return fetchProduct().then((item) => {
-      expect(item).toEqual({ item: "Milk", price: 1000 });
+      expect(item).toEqual({ item: "Milk", price: 2000 });
     });
   });
 });
