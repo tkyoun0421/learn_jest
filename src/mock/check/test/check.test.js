8c921c9930f5f70db30dae1f36d88bb80090c5ff
태관 <tkyoun0421@naver.com>
2024-06-19 15:16:59 +0900

    mock에 관해서

--- src/mock/check/test/check.test.js
+++ src/mock/check/test/check.test.js
@@ -8,4 +8,26 @@ describe("check", () => {
     onSuccess = jest.fn();
     onFail = jest.fn();
   });
+
+  it("should call onSuccess when predicate is true", () => {
+    check(() => true, onSuccess, onFail);
+
+    // expect(onSuccess.mock.calls.length).toBe(1);
+    expect(onSuccess).toHaveBeenCalledTimes(1);
+    // expect(onSuccess.mock.calls[0][0]).toBe("Yes");
+    expect(onSuccess).toHaveBeenCalledWith("Yes");
+    // expect(onFail.mock.calls.length).toBe(0);
+    expect(onFail).toHaveBeenCalledTimes(0);
+  });
+
+  it("should call onFail when predicate is false", () => {
+    check(() => false, onSuccess, onFail);
+
+    // expect(onSuccess.mock.calls.length).toBe(1);
+    expect(onFail).toHaveBeenCalledTimes(1);
+    // expect(onSuccess.mock.calls[0][0]).toBe("Yes");
+    expect(onFail).toHaveBeenCalledWith("No");
+    // expect(onFail.mock.calls.length).toBe(0);
+    expect(onSuccess).toHaveBeenCalledTimes(0);
+  });
 });
