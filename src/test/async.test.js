740f7c7e0d3863e242204e4fe90c07aa39f8ec5d
태관 <tkyoun0421@naver.com>
2024-06-19 15:05:34 +0900

    비동기 테스트

--- src/test/async.test.js
+++ src/test/async.test.js
@@ -12,4 +12,18 @@ describe("Async", () => {
       expect(item).toEqual({ item: "Milk", price: 2000 });
     });
   });
+  it("async-await", async () => {
+    const product = await fetchProduct();
+    expect(product).toEqual({ item: "Milk", price: 2000 });
+  });
+
+  it("async-resolves", () => {
+    return expect(fetchProduct()).resolves.toEqual({
+      item: "Milk",
+      price: 2000,
+    });
+  });
+  it("async-reject", () => {
+    return expect(fetchProduct("error")).rejects.toBe("network error!");
+  });
 });
