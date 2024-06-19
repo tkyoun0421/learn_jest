a5da23ee517ef0a5e469b3ce22210cef1e4df9fe
태관 <tkyoun0421@naver.com>
2024-06-19 14:20:34 +0900

    계산기 테스트 코드 구현

---
+++ src/test/calculator.test.js
@@ -0,0 +1,47 @@
+const Calculator = require("../calculator");
+
+describe("Calculator", () => {
+  let calc;
+  beforeEach(() => {
+    calc = new Calculator();
+  });
+  it("inits with 0", () => {
+    expect(calc.value).toBe(0);
+  });
+
+  it("sets", () => {
+    calc.set(9);
+    expect(calc.value).toBe(9);
+  });
+
+  it("clears", () => {
+    calc.set(9);
+    calc.clear();
+    expect(calc.value).toBe(0);
+  });
+
+  it("adds", () => {
+    calc.set(1);
+    calc.add(3);
+    expect(calc.value).toBe(4);
+  });
+
+  it("multiplies", () => {
+    calc.set(1);
+    calc.multiply(3);
+    expect(calc.value).toBe(3);
+  });
+
+  describe("divides", () => {
+    it("0 / 0 === NaN", () => {
+      calc.divide(0);
+      expect(calc.value).toBe(NaN);
+    });
+
+    it("1 / 0 === Infinity", () => {
+      calc.set(1);
+      calc.divide(0);
+      expect(calc.value).toBe(Infinity);
+    });
+  });
+});
